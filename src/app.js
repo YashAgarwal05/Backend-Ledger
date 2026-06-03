7ddd85dda6e47249f2bcabe2c89ce23f99755fb5
Yash Agarwal <yashagarwalalwar@gmail.com>
2026-06-04 02:31:28 +0530

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   src/Controllers/account.controller.js 	new file:   src/Controllers/auth.controller.js 	new file:   src/Controllers/transaction.controller.js 	modified:   src/app.js 	new file:   src/middleware/auth.middleware.js 	new file:   src/models/account.model.js 	new file:   src/models/blackList.model.js 	new file:   src/models/ledger.model.js 	new file:   src/models/transaction.model.js 	new file:   src/models/user.model.js 	new file:   src/routes/account.routes.js 	new file:   src/routes/auth.routes.js 	new file:   src/routes/transaction.routes.js 	new file:   src/services/email.service.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
-const express=require("express");
+const express = require("express")
+const cookieParser = require("cookie-parser")
 
 
 
-const app=express();
+const app = express()
 
 
+app.use(express.json())
+app.use(cookieParser())
 
-module.exports=app
+/**
+ * - Routes required
+ */
+const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+const transactionRoutes = require("./routes/transaction.routes")
+
+/**
+ * - Use Routes
+ */
+
+app.get("/", (req, res) => {
+    res.send("Ledger Service is up and running")
+})
+
+app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
+app.use("/api/transactions", transactionRoutes)
+
+module.exports = app
